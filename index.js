6a9835c08cea243e7b292b69f20e2ab88d8eb380
rosajose11 <rosajose1411@gmail.com>
2025-06-06 14:54:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,70 @@
-const http=require('http');
-const fs=require('fs');
-const { isBuffer } = require('util');
+let userForm=document.getElementById('user_form');
 
-let homecontent='';
-let projectcontent='';
-fs.readFile('home.html',(err,home)=>{
-    if(err){
-        throw err;
-    
-    }
-    homecontent=home;
+const retreiveUserEntries=()=>{
+  let entries=localStorage.getItem("user-Entries");
+  if(entries){
+    entries=JSON.parse(entries);
+  }
+  else{
+    entries=[];
+  }
+  return entries;
+}
+function isvaliddob(dob){
+  const birthDate=new Date(dob);
+  const today=new Date();
+  let age=today.getFullYear()-birthDate.getFullYear();
+  const monthDiff=today.getMonth()-birthDate.getMonth();
+  if(monthDiff<0 || (monthDiff===0 && today.getDate()<birthDate.getDate())){
+    age--;
+  }
+  return age>=18 && age<=55;
+}
 
-});
-
-fs.readFile('project.html',(err,project)=>{
-    if(err){
-        throw err;
-    }
-    projectcontent=project;
-});
-
-
-
-http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectcontent);
-        response.end();
-        break;
-      default:
-        response.write(homecontent);
-        response.end();
-        break;
-    }
-  })
-  .listen(3000);
+let userEntries=retreiveUserEntries();
+const displayUserEntries=()=>{
+  const entries= retreiveUserEntries();
+  const TableEntries=entries.map((entry)=>{
+    const nameCell=`<td class='border px-4 py-2'>${entry.name}</td>`;
+    const emailCell=`<td class='border px-4 py-2'>${entry.email}</td>`;
+    const passwordCell=`<td class='border px-4 py-2'>${entry.password}</td>`;
+    const dobCell=`<td class='border px-4 py-2'>${entry.dob}</td>`;
+    const acceptedTermsAndConditionsCell=`<td class='border px-4 py-2'>${entry.acceptedTermsAndConditions}</td>`;
+    const row=`<tr>${nameCell}${emailCell}${passwordCell}${dobCell}${acceptedTermsAndConditionsCell}</tr>`;
+    return row;
+  }).join("\n");
+  const table=`<table class='table-auto w-full'><tr>
+  <th class="px-4 py-2">Name</th>
+  <th class="px-4 py-2">Email</th>
+  <th class="px-4 py-2">Password</th>
+  <th class="px-4 py-2">Date of Birth</th>
+  <th class="px-4 py-2">Accepted Terms and Conditions</th>
+  </tr>
+  ${TableEntries}</table>`;
+  let details=document.getElementById('user-entries');
+  details.innerHTML=table;
+}
+const saveUserForm=(event)=>{
+  event.preventDefault();
+  const name=document.getElementById('name').value;
+  const email=document.getElementById('email').value;
+  const password=document.getElementById('password').value;
+  const dob=document.getElementById('dob').value;
+  const acceptedTermsAndConditions=document.getElementById('terms').checked;
+  if(!isvaliddob(dob)){
+    alert("You must be between 18 and 55 years old to register.");
+    return;
+  }
+  const entry={
+    name,
+    email,
+    password,
+    dob,
+    acceptedTermsAndConditions
+  };
+  userEntries.push(entry);
+  localStorage.setItem('user-Entries',JSON.stringify(userEntries));
+  displayUserEntries();
+}
+userForm.addEventListener('submit',saveUserForm);
+displayUserEntries();
